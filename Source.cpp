2d9320d665e75777691ca594232c29b96625dae3
myasgari <mohamadyasinasgari@gmail.com>
2019-02-01 18:22:59 +0330

    the neccessary funct added for reducing code frome sourse

--- Source.cpp
+++ Source.cpp
@@ -2,11 +2,14 @@
 
 using namespace std;
 enum choise { CREATE = 1, ERASE, ERASEALL, LIST, EXPORT, ANIMTE, ANIMTELIST, ERAESEANIM };
-void create(vector<shape *> &);
-void list(vector<shape *>&);
-void erase(vector<shape *>&)
 int enterChois();
+void create(vector<shape *>&);
+void list(const vector<shape *>&);
+void erase(vector<shape *>&);
 void createFile(vector<shape *>&,fstream &);
+void animate(vector<shape *>&);
+void animlist(vector<shape *>&);
+void eraseanim(vector<shape *>&);
 int main()
 {
 
