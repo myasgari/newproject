45792dcacf60c91fbba35589036fa87cfbccc34a
myasgari <mohamadyasinasgari@gmail.com>
2019-02-01 18:24:48 +0330

    the switch handeler added ,file close ,the allocter memory deleted

--- Source.cpp
+++ Source.cpp
@@ -20,5 +20,41 @@ int main()
 		}
 		vector<shape *>eraseVector;
 		vector<shape *>shape;
+		while  ((choice = enterChois()) != 9)
+	{
+		switch (choice)
+		{
+		case CREATE:
+			create(shape);
+			break;
+		case ERASE:
+			erase(shape);
+			break;
+		case ERASEALL:
+			shape = eraseVector;
+			break;
+		case LIST:
+			list(shape);
+			break;
+		case EXPORT:
+			createFile(shape, pic);
+			break;
+		case ANIMTE:
+			animate(shape);
+			break;
+		case ANIMTELIST:
+			animlist(shape);
+			break;
+		case ERAESEANIM:
+			eraseanim(shape);
+			break;
+		default:
+			cout << "the number that you enter wrong plz try again" << endl;
+		}
+	}
+	for (int i = 0; i < shape.size(); i++)
+		delete shape[i];
+	pic.close();
+	return 0;
 	return 0;
 }
