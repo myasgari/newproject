f066dda0e7562194f4d07a89fb4745bd8485ff4e
myasgari <mohamadyasinasgari@gmail.com>
2019-02-04 18:34:42 +0330

    the funct added with using string funct

--- prosess.cpp
+++ prosess.cpp
@@ -8,7 +8,7 @@
 //#include "polyline.h"
 //#include "scatter.h"
 //#include "scatterline.h"
-//#include "atter.h"
+#include "atterb.h"
 #include <vector>
 #include <fstream>
 #include <string>
@@ -35,5 +35,20 @@ void create(const string &enter, vector<shape *>&shape)
 }
 void set(const string &enter, vector<shape *>&shape)
 {
-
+	int i = 0;
+	atterb *a = new atterb();
+	for (i; i < shape.size(); i++)
+	{
+		string name = shape[i]->returnName();
+		if (enter.find(name) != string::npos)
+			break;
+	}
+	int posion = enter.find_first_of('>');
+	int posions = enter.find_first_of('(');
+	string newkey = enter.substr(posion + 1, posions - posion - 1);
+	int posionss = enter.find_last_of(')');
+	string newvalue= enter.substr(posions + 1, posionss - posions - 1);
+	a->key = newkey;
+	a->value = newvalue;
+	shape[i]->atter.push_back(a);
 }
