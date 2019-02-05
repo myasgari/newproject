140795d9b3c8ce5908cef4aac5b99c2b5e77d76a
myasgari <mohamadyasinasgari@gmail.com>
2019-02-05 19:23:49 +0330

    the protatype of header write

---
+++ rect.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "shape.h"
+#include <string>
+class rect :public shape
+{
+public:
+	rect(const std::string &);
+	virtual std::string returnName() override;
+	virtual std::string Export() override;
+	~rect();
+private:
+	std::string Name;
+	std::string write;
+};
