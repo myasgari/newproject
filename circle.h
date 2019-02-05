23b2f828891d34dec0bd84ee186d152babe5e8f8
myasgari <mohamadyasinasgari@gmail.com>
2019-02-05 21:12:15 +0330

    the funct get add

--- circle.h
+++ circle.h
@@ -7,6 +7,7 @@ public:
 	circle(const std::string &);
 	virtual std::string returnName() override;
 	virtual std::string Export() override ;
+	virtual std::string get(const std::string &) override;
 	~circle();
 private:
 	std::string Name;
