aae0ffc7f7525ffc59de19cc27ac305e06a935ff
myasgari <mohamadyasinasgari@gmail.com>
2019-02-06 08:48:55 +0330

    the protaype of funct write

---
+++ polyline.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "shape.h"
+#include <string>
+class polyline :public shape
+{
+public:
+	polyline(const std::string &);
+	virtual std::string returnName() override;
+	virtual std::string Export() override;
+	virtual std::string get(const std::string &) override;
+	~polyline();
+private:
+	std::string Name;
+	std::string write;
+};
