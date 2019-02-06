1e48c61ce3ed19af4a19c0f8c81c59552ee9225b
myasgari <mohamadyasinasgari@gmail.com>
2019-02-06 08:03:44 +0330

    the funct of line write

---
+++ line.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "shape.h"
+#include <string>
+class line:public shape
+{
+public:
+	line(const std::string &);
+	virtual std::string returnName() override;
+	virtual std::string Export() override;
+	virtual std::string get(const std::string &) override;
+	~line();
+private:
+	std::string Name;
+	std::string write;
+};
